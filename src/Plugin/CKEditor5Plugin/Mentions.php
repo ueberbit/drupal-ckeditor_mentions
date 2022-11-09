2b37d3958df2abfc202f8c8009fc8194d9f5cb9c
Florian Weber <florian.weber@ueberbit.de>
2022-11-09 21:06:53 +0100

    Allow multiple mention plugins per text format

--- src/Plugin/CKEditor5Plugin/Mentions.php
+++ src/Plugin/CKEditor5Plugin/Mentions.php
@@ -40,8 +40,6 @@ class Mentions extends CKEditor5PluginDefault implements CKEditor5PluginConfigur
 
   use CKEditor5PluginConfigurableTrait;
 
-  const DEFAULT_PLUGIN = 'user';
-
   /**
    * Mentions Plugin Manager.
    *
@@ -56,39 +54,6 @@ class Mentions extends CKEditor5PluginDefault implements CKEditor5PluginConfigur
     $instance = new static($configuration, $plugin_id, $plugin_definition);
     $instance->mentionsPluginManager = $container->get('plugin.manager.mentions_type');
     return $instance;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getConfig(Editor $editor) {
-    $settings = $editor->getSettings()['plugins']['mentions'];
-
-    $type = $settings['mentions_type'];
-    /** @var \Drupal\ckeditor_mentions\MentionsType\MentionsTypeBase $plugin */
-    $plugin = $this->mentionsPluginManager->createInstance($type);
-
-    $config = [
-      'mentions' => [
-        [
-          'throttle' => $settings['timeout'],
-          'minChars' => $settings['charcount'],
-          'feed' => '/ckeditor-mentions/ajax/' . $type . '/{encodedQuery}',
-          'itemTemplate' => $plugin->getItemTemplate(),
-          'outputTemplate' => $plugin->getOutputTemplate(),
-          'marker' => $settings['marker'],
-        ],
-      ],
-    ];
-
-    if ($settings['use_advanced_pattern']) {
-      $config['mentions'][0]['pattern'] = $settings['advanced_pattern'];
-    }
-    elseif ($settings['pattern']) {
-      $config['mentions'][0]['pattern'] = $this->generatePattern($settings);
-    }
-
-    return $config;
   }
 
   /**
@@ -120,142 +85,106 @@ class Mentions extends CKEditor5PluginDefault implements CKEditor5PluginConfigur
    * {@inheritdoc}
    */
   public function buildConfigurationForm(array $form, FormStateInterface $form_state): array {
-    $settings = $this->getConfiguration();
+    $settings = $this->configuration;
 
-    $form['enable'] = [
-      '#type' => 'checkbox',
-      '#title' => $this->t('Enable Mentions'),
-      '#default_value' => !empty($settings['enable']) ? $settings['enable'] : FALSE,
-      '#attributes' => ['data-use-mentions' => TRUE],
-    ];
+    foreach ($this->mentionsPluginManager->getAllMentionsTypes() as $mentionsType => $mentionsLabel) {
+      $form['plugins'][$mentionsType]['#type'] = 'fieldset';
+      $form['plugins'][$mentionsType]['enable'] = [
+        '#type' => 'checkbox',
+        '#title' => $this->t('Enable Mentions: %type', ['%type' => $mentionsLabel]),
+        '#default_value' => !empty($settings['plugins'][$mentionsType]['enable']) ? $settings['plugins'][$mentionsType]['enable'] : FALSE,
+        '#attributes' => ['data-use-mentions--' . $mentionsType => TRUE],
+      ];
 
-    $default_plugin_id = !empty($settings['mentions_type']) ? $settings['mentions_type'] : self::DEFAULT_PLUGIN;
+      $form['plugins'][$mentionsType]['timeout'] = [
+        '#type' => 'number',
+        '#min' => 1,
+        '#title' => $this->t('Timeout (milliseconds)'),
+        '#description' => $this->t('Enter time in milliseconds for mentions script to stop checking for matches.'),
+        '#default_value' => !empty($settings['plugins'][$mentionsType]['timeout']) ? $settings['plugins'][$mentionsType]['timeout'] : 500,
+        '#states' => [
+          'visible' => [
+            ':input[data-use-mentions--' . $mentionsType . ']' => ['checked' => TRUE],
+          ],
+        ]
+      ];
 
-    $form['mentions_type'] = [
-      '#type' => 'select',
-      '#options' => $this->mentionsPluginManager->getAllMentionsTypes(),
-      '#default_value' => $default_plugin_id,
-      '#description' => $this->t('Choose the mentions plugin'),
-    ];
-
-    $form['timeout'] = [
-      '#type' => 'number',
-      '#min' => 1,
-      '#title' => $this->t('Timeout (milliseconds)'),
-      '#description' => $this->t('Enter time in milliseconds for mentions script to stop checking for matches.'),
-      '#default_value' => !empty($settings['timeout']) ? $settings['timeout'] : 500,
-    ];
-
-    $form['advanced'] = [
-      '#type' => 'details',
-      '#parents' => ['editor', 'settings', 'plugins', 'ckeditor_mentions_mentions'],
-      '#tree' => TRUE,
-      '#title' => $this->t('Advanced settings'),
-      '#open' => FALSE,
-    ];
-
-    $form['advanced']['marker'] = [
-      '#type' => 'textfield',
-      '#maxlength' => '1',
-      '#title' => $this->t('Marker'),
-      '#description' => $this->t('The character that should trigger autocompletion.'),
-      '#default_value' => !empty($settings['marker']) ? $settings['marker'] : '@',
-      '#states' => [
-        'visible' => [
-          ':input[data-use-advanced]' => ['checked' => FALSE],
+      $form['plugins'][$mentionsType]['marker'] = [
+        '#type' => 'textfield',
+        '#maxlength' => '1',
+        '#title' => $this->t('Marker'),
+        '#description' => $this->t('The character that should trigger autocompletion.'),
+        '#default_value' => !empty($settings['plugins'][$mentionsType]['marker']) ? $settings['plugins'][$mentionsType]['marker'] : '@',
+        '#states' => [
+          'visible' => [
+            ':input[data-use-advanced--' . $mentionsType . ']' => ['checked' => FALSE],
+            ':input[data-use-mentions--' . $mentionsType . ']' => ['checked' => TRUE],
+          ],
+          'required' => [
+            ':input[data-use-advanced--' . $mentionsType . ']' => ['checked' => FALSE],
+            ':input[data-use-mentions--' . $mentionsType . ']' => ['checked' => TRUE],
+          ],
         ],
-        'required' => [
-          ':input[data-use-advanced]' => ['checked' => FALSE],
-          ':input[data-use-mentions]' => ['checked' => TRUE],
-        ],
-      ],
-    ];
+      ];
 
-    $form['advanced']['charcount'] = [
-      '#type' => 'number',
-      '#min' => 0,
-      '#title' => $this->t('Character Count'),
-      '#description' => $this->t('Enter minimum number of characters that must be typed to trigger mention match.'),
-      '#default_value' => !empty($settings['charcount']) ? $settings['charcount'] : 2,
-      '#states' => [
-        'visible' => [
-          ':input[data-use-advanced]' => ['checked' => FALSE],
+      $form['plugins'][$mentionsType]['charcount'] = [
+        '#type' => 'number',
+        '#min' => 0,
+        '#title' => $this->t('Character Count'),
+        '#description' => $this->t('Enter minimum number of characters that must be typed to trigger mention match.'),
+        '#default_value' => !empty($settings['plugins'][$mentionsType]['charcount']) ? $settings['plugins'][$mentionsType]['charcount'] : 2,
+        '#states' => [
+          'visible' => [
+            ':input[data-use-advanced--' . $mentionsType . ']' => ['checked' => FALSE],
+            ':input[data-use-mentions--' . $mentionsType . ']' => ['checked' => TRUE],
+          ],
         ],
-      ],
-    ];
+      ];
 
-    $form['advanced']['use_advanced_pattern'] = [
-      '#type' => 'checkbox',
-      '#title' => $this->t('Use advanced pattern?'),
-      '#default_value' => !empty($settings['use_advanced_pattern']) ? $settings['use_advanced_pattern'] : FALSE,
-      '#attributes' => ['data-use-advanced' => TRUE],
-    ];
-
-    $form['advanced']['pattern'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Pattern'),
-      '#description' => $this->t('The pattern used to match queries. <br /> Leave empty to use default CKeditor pattern. <br/> <em>Note: you should not include marker and min characters in pattern, they will be automatically generated. Use advanced pattern for this.</em><br /> Example: Pattern to match Cyrillic characters: <code>[а-яА-Я]</code>'),
-      '#default_value' => !empty($settings['pattern']) ? $settings['pattern'] : '',
-      '#states' => [
-        'visible' => [
-          ':input[data-use-advanced]' => ['checked' => FALSE],
+      $form['plugins'][$mentionsType]['use_advanced_pattern'] = [
+        '#type' => 'checkbox',
+        '#title' => $this->t('Use advanced pattern?'),
+        '#default_value' => !empty($settings['plugins'][$mentionsType]['use_advanced_pattern']) ? $settings['plugins'][$mentionsType]['use_advanced_pattern'] : FALSE,
+        '#attributes' => ['data-use-advanced--' . $mentionsType => TRUE],
+        '#states' => [
+          'visible' => [
+            ':input[data-use-mentions--' . $mentionsType . ']' => ['checked' => TRUE],
+          ],
         ],
-      ],
-    ];
+      ];
 
-    $form['advanced']['advanced_pattern'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Advanced pattern'),
-      '#default_value' => !empty($settings['advanced_pattern']) ? $settings['advanced_pattern'] : '',
-      '#description' => $this->t('Be careful! You should include marker and characters count in this pattern by yourself! <br /> Example: Pattern to match two words with space in between. <code>@([_a-zA-Z0-9À-ž]{2,}\s?)([_a-zA-Z0-9À-ž]*)?$</code>'),
-      '#states' => [
-        'visible' => [
-          ':input[data-use-advanced]' => ['checked' => TRUE],
+      $form['plugins'][$mentionsType]['pattern'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Pattern'),
+        '#description' => $this->t('The pattern used to match queries. <br /> Leave empty to use default CKeditor pattern. <br/> <em>Note: you should not include marker and min characters in pattern, they will be automatically generated. Use advanced pattern for this.</em><br /> Example: Pattern to match Cyrillic characters: <code>[а-яА-Я]</code>'),
+        '#default_value' => !empty($settings['plugins'][$mentionsType]['pattern']) ? $settings['plugins'][$mentionsType]['pattern'] : '',
+        '#states' => [
+          'visible' => [
+            ':input[data-use-advanced--' . $mentionsType . ']' => ['checked' => FALSE],
+            ':input[data-use-mentions--' . $mentionsType . ']' => ['checked' => TRUE],
+          ],
         ],
-        'required' => [
-          ':input[data-use-advanced]' => ['checked' => TRUE],
-          ':input[data-use-mentions]' => ['checked' => TRUE],
-        ],
-      ],
-    ];
+      ];
 
-    $form['mentions_type']['#element_validate'][] = [$this, 'isRequired'];
+      $form['plugins'][$mentionsType]['advanced_pattern'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Advanced pattern'),
+        '#default_value' => !empty($settings['plugins'][$mentionsType]['advanced_pattern']) ? $settings['plugins'][$mentionsType]['advanced_pattern'] : '',
+        '#description' => $this->t('Be careful! You should include marker and characters count in this pattern by yourself! <br /> Example: Pattern to match two words with space in between. <code>@([_a-zA-Z0-9À-ž]{2,}\s?)([_a-zA-Z0-9À-ž]*)?$</code>'),
+        '#states' => [
+          'visible' => [
+            ':input[data-use-advanced--' . $mentionsType . ']' => ['checked' => TRUE],
+            ':input[data-use-mentions--' . $mentionsType . ']' => ['checked' => TRUE],
+          ],
+          'required' => [
+            ':input[data-use-advanced--' . $mentionsType . ']' => ['checked' => TRUE],
+            ':input[data-use-mentions--' . $mentionsType . ']' => ['checked' => TRUE],
+          ],
+        ],
+      ];
+    }
 
     return $form;
-  }
-
-  /**
-   * Check if plugin is enabled.
-   *
-   * @param array $element
-   *   The Form element.
-   * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   The FormState Object.
-   */
-  public function isRequired(array $element, FormStateInterface $form_state) {
-    $settings = $this->getMentionsStateValues($form_state);
-    $parents = $element['#parents'];
-    $element_name = array_pop($parents);
-
-    if ($settings['enable'] && !$settings[$element_name]) {
-      $form_state->setError($element, $this->t('@type field is required.', ['@type' => $element_name]));
-    }
-  }
-
-  /**
-   * Get current values from form state.
-   *
-   * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   Form state.
-   *
-   * @return array
-   *   Values
-   */
-  protected function getMentionsStateValues(FormStateInterface $form_state) {
-    return $form_state instanceof SubformState ?
-      $form_state->getCompleteFormState()
-        ->getValue(['editor', 'settings', 'plugins', 'ckeditor_mentions_mentions']) :
-      $form_state->getValue(['editor', 'settings', 'plugins', 'ckeditor_mentions_mentions']);
   }
 
   /**
@@ -263,14 +192,7 @@ class Mentions extends CKEditor5PluginDefault implements CKEditor5PluginConfigur
    */
   public function defaultConfiguration() {
     return [
-      'enable' => FALSE,
-      'mentions_type' => self::DEFAULT_PLUGIN,
-      'timeout' => 500,
-      'marker' => '@',
-      'charcount' => 2,
-      'use_advanced_pattern' => FALSE,
-      'pattern' => '',
-      'advanced_pattern' => '',
+      'plugins' => [],
     ];
   }
 
@@ -282,48 +204,61 @@ class Mentions extends CKEditor5PluginDefault implements CKEditor5PluginConfigur
    * {@inheritdoc}
    */
   public function submitConfigurationForm(array &$form, FormStateInterface $form_state): void {
-    $this->configuration['enable'] = (bool) $form_state->getValue('enable');
-    $this->configuration['mentions_type'] = $form_state->getValue('mentions_type');
-    $this->configuration['timeout'] = (int) $form_state->getValue('timeout');
-    $this->configuration['marker'] = $form_state->getValue('marker');
-    $this->configuration['charcount'] = (int) $form_state->getValue('charcount');
-    $this->configuration['use_advanced_pattern'] = (bool) $form_state->getValue('use_advanced_pattern');
-    $this->configuration['pattern'] = $form_state->getValue('pattern');
-    $this->configuration['advanced_pattern'] = $form_state->getValue('advanced_pattern');
+    $configuration = [];
+
+    foreach ($this->mentionsPluginManager->getAllMentionsTypes() as $mentionsType => $mentionsLabel) {
+      $mentionsTypeConfiguration = $form_state->getValue([
+        'plugins',
+        $mentionsType
+      ]);
+      if ($mentionsTypeConfiguration['enable']) {
+        $mentionsTypeConfiguration['id'] = $mentionsType;
+        $mentionsTypeConfiguration['enable'] = (bool) $mentionsTypeConfiguration['enable'];
+        $mentionsTypeConfiguration['charcount'] = (int) $mentionsTypeConfiguration['charcount'];
+        $mentionsTypeConfiguration['timeout'] = (int) $mentionsTypeConfiguration['timeout'];
+        $mentionsTypeConfiguration['use_advanced_pattern'] = (bool) $mentionsTypeConfiguration['use_advanced_pattern'];
+        $configuration[$mentionsType] = $mentionsTypeConfiguration;
+      }
+    }
+
+    $this->configuration['plugins'] = $configuration;
   }
 
   public function getDynamicPluginConfig(array $static_plugin_config, EditorInterface $editor): array {
-    if (!$this->configuration['enable']) {
-      return [];
+    $options = [];
+
+    foreach ($this->configuration['plugins'] as $settings) {
+      $options['feeds'][] = [
+        'marker' => $settings['marker'],
+        'feed' => [
+          'func' => [
+            'name' => 'Drupal.ckeditor5DrupalMention',
+            'args' => [
+              [
+                'type' => $settings['id'],
+                'url' => Url::fromRoute('ckeditor_mentions.ajax_callback', [
+                  'plugin_id' => $settings['id'],
+                  'match' => '--match--',
+                ])->setAbsolute()->toString(),
+                'marker' => $settings['marker'],
+              ],
+            ],
+            'invoke' => TRUE,
+          ],
+        ],
+        'itemRenderer' => [
+          'func' => [
+            'name' => 'Drupal.ckeditor5DrupalMentionUser.itemRenderer',
+          ],
+        ],
+        'minimumCharacters' => $settings['charcount'],
+        'drupalMentionsType' => $settings['id'],
+      ];
     }
 
-    $options = [
-      'feeds' => [
-        [
-          'marker' => $this->configuration['marker'],
-          'feed' => [
-            'func' => [
-              'name' => 'Drupal.ckeditor5DrupalMention',
-              'args' => [
-                [
-                  'type' => 'user',
-                  'url' => Url::fromRoute('ckeditor_mentions.ajax_callback', ['plugin_id' => 'user', 'match' => '--match--'])->setAbsolute()->toString(),
-                  'marker' => $this->configuration['marker'],
-                ],
-              ],
-              'invoke' => TRUE,
-            ],
-          ],
-          'itemRenderer' => [
-            'func' => [
-              'name' => 'Drupal.ckeditor5DrupalMentionUser.itemRenderer',
-            ],
-          ],
-          'minimumCharacters' => $this->configuration['charcount'],
-          'drupalMentionsType' => 'user',
-        ],
-      ],
-    ];
+    if (!$options) {
+      return [];
+    }
 
     return ['mention' => $options];
   }
